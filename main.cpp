c9aabd8ff1b4ec6a4754190aaf5b4f2843c91cba
maxim1812 <ppppp.ppppppp2018@yandex.ru>
2017-02-25 18:30:43 +0300

    Cin two numbers and trace summa
    It is written program

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// my version 1
+
+int main(int argc, char** argv) 
+{
+	int a,b,c;
+	
+	cin >> a;
+	cin >> b;
+	
+	c = a + b;
+	cout << c << "\n";
+	
+	return 0;
+}
